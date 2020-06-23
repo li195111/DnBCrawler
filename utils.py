aeb88b8b51ad974f3939fc8794018f6585431463
li195111 <green07111@gmail.com>
2020-06-23 12:03:01 +0800

    update

--- utils.py
+++ utils.py
@@ -17,6 +17,8 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+WAIT_TIME = 0.5
+
 Industry_TABLE_SYNTAX ='''
 CREATE TABLE IF NOT EXISTS %s (
     ID INT AUTO_INCREMENT PRIMARY KEY,
@@ -114,10 +116,10 @@ def CREATE_DB(DB_NAME, host= 'localhost', user= 'root', password= 'Aboutx_121'):
                 port= 3306)
         except mysql.connector.DatabaseError as e:
             print (f"{bcolors.FAIL}Error:{bcolors.OKGREEN}\t{e}{bcolors.ENDC}")
-            time.sleep(0.1)
+            time.sleep(WAIT_TIME)
         except mysql.connector.OperationalError as e:
             print (f"{bcolors.FAIL}Error:{bcolors.OKGREEN}\t{e}{bcolors.ENDC}")
-            time.sleep(0.1)
+            time.sleep(WAIT_TIME)
         try:
             if connect.is_connected():
                 break
@@ -138,10 +140,10 @@ def CHECK_DB(DB_NAME, host= 'localhost', user= 'root', password= 'Aboutx_121'):
                 port= 3306)
         except mysql.connector.DatabaseError as e:
             print (f"{bcolors.FAIL}Error:{bcolors.OKGREEN}\t{e}{bcolors.ENDC}")
-            time.sleep(0.1)
+            time.sleep(WAIT_TIME)
         except mysql.connector.OperationalError as e:
             print (f"{bcolors.FAIL}Error:{bcolors.OKGREEN}\t{e}{bcolors.ENDC}")
-            time.sleep(0.1)
+            time.sleep(WAIT_TIME)
         try:
             if connect.is_connected():
                 break
@@ -163,10 +165,10 @@ def DROP_DB(DB_NAME, host= 'localhost', user= 'root', password= 'Aboutx_121'):
                 port= 3306)
         except mysql.connector.DatabaseError as e:
             print (f"{bcolors.FAIL}Error:{bcolors.OKGREEN}\t{e}{bcolors.ENDC}")
-            time.sleep(0.1)
+            time.sleep(WAIT_TIME)
         except mysql.connector.OperationalError as e:
             print (f"{bcolors.FAIL}Error:{bcolors.OKGREEN}\t{e}{bcolors.ENDC}")
-            time.sleep(0.1)
+            time.sleep(WAIT_TIME)
         try:
             if connect.is_connected():
                 break
@@ -189,10 +191,10 @@ def CREATE_TB(TB_NAME, TB_SYNTAX, DB_NAME, host= 'localhost', user= 'root', pass
             
         except mysql.connector.DatabaseError as e:
             print (f"{bcolors.FAIL}Error:{bcolors.OKGREEN}\t{e}{bcolors.ENDC}")
-            time.sleep(0.1)
+            time.sleep(WAIT_TIME)
         except mysql.connector.OperationalError as e:
             print (f"{bcolors.FAIL}Error:{bcolors.OKGREEN}\t{e}{bcolors.ENDC}")
-            time.sleep(0.1)
+            time.sleep(WAIT_TIME)
         try:
             if connect.is_connected():
                 break
@@ -220,10 +222,10 @@ def INSERT(DB_NAME:str, TB_NAME:str, INSERT_VAL:list, host= 'localhost', user= '
             
         except mysql.connector.DatabaseError as e:
             print (f"{bcolors.FAIL}Error:{bcolors.OKGREEN}\t{e}{bcolors.ENDC}")
-            time.sleep(0.1)
+            time.sleep(WAIT_TIME)
         except mysql.connector.OperationalError as e:
             print (f"{bcolors.FAIL}Error:{bcolors.OKGREEN}\t{e}{bcolors.ENDC}")
-            time.sleep(0.1)
+            time.sleep(WAIT_TIME)
         try:
             if connect.is_connected():
                 break
@@ -271,10 +273,10 @@ def GetItemID(DB_NAME, TB_NAME, ITEM_VALUES, host= 'localhost', user= 'root', pa
             
         except mysql.connector.DatabaseError as e:
             print (f"{bcolors.FAIL}Error:{bcolors.OKGREEN}\t{e}{bcolors.ENDC}")
-            time.sleep(0.1)
+            time.sleep(WAIT_TIME)
         except mysql.connector.OperationalError as e:
             print (f"{bcolors.FAIL}Error:{bcolors.OKGREEN}\t{e}{bcolors.ENDC}")
-            time.sleep(0.1)
+            time.sleep(WAIT_TIME)
         try:
             if connect.is_connected():
                 break
@@ -310,10 +312,10 @@ def SelectItems(DB_NAME:str, TB_NAME:str, SELECT_ITEMS, ITEM_VALUES, host= 'loca
             
         except mysql.connector.DatabaseError as e:
             print (f"{bcolors.FAIL}Error:{bcolors.OKGREEN}\t{e}{bcolors.ENDC}")
-            time.sleep(0.1)
+            time.sleep(WAIT_TIME)
         except mysql.connector.OperationalError as e:
             print (f"{bcolors.FAIL}Error:{bcolors.OKGREEN}\t{e}{bcolors.ENDC}")
-            time.sleep(0.1)
+            time.sleep(WAIT_TIME)
         try:
             if connect.is_connected():
                 break
@@ -347,10 +349,10 @@ def DeleteItem(DB_NAME:str, TB_NAME:str, SELECT_ITEMS, ITEM_VALUES, host= 'local
             
         except mysql.connector.DatabaseError as e:
             print (f"{bcolors.FAIL}Error:{bcolors.OKGREEN}\t{e}{bcolors.ENDC}")
-            time.sleep(0.1)
+            time.sleep(WAIT_TIME)
         except mysql.connector.OperationalError as e:
             print (f"{bcolors.FAIL}Error:{bcolors.OKGREEN}\t{e}{bcolors.ENDC}")
-            time.sleep(0.1)
+            time.sleep(WAIT_TIME)
         try:
             if connect.is_connected():
                 break
