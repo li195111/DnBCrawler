407f201511fd5084a22dfd465dbb01526813085b
li195111 <green07111@gmail.com>
2020-06-23 15:22:29 +0800

    update

--- spiders/dunbradstreet_region_sql.py
+++ spiders/dunbradstreet_region_sql.py
@@ -86,62 +86,62 @@ if __name__ == "__main__":
     ALL_INDUSTRY = os.listdir(INDUSTRY_DIR)
     INIT_DB(DB_NAME)
     num_industry = len(ALL_INDUSTRY)
-    # for i in range(num_industry):
-    if len(sys.argv) > 1:
-        i = int(sys.argv[1])
-    else:
-        i = 3
-    if len(sys.argv) > 2:
-        limite = int(sys.argv[2])
-    else:
-        limite = 2
-    while True:
-        IndustryID = GetItemID(DB_NAME, "Industry", [i+1])
-        category_datas = SelectItems(DB_NAME, "Category", "IndustryID,", [IndustryID,])
-        numCategorys = len(category_datas)
-        NewLocationDatas = []
-        region_name = ""
-        for idx, category_data in enumerate(category_datas):
-            CategoryID = category_data[0]
-            if len(NewLocationDatas) >= limite:
-                break
-            region_datas = SelectItems(DB_NAME, "Region", "CategoryID,", [CategoryID,])
-            num_reg_datas = len(region_datas)
-            for reg_idx, region_data in enumerate(region_datas):
-                RegionID = region_data[0]
+    for i in range(num_industry):
+    # if len(sys.argv) > 1:
+    #     i = int(sys.argv[1])
+    # else:
+    #     i = 0
+        if len(sys.argv) > 2:
+            limite = int(sys.argv[2])
+        else:
+            limite = 2
+        while True:
+            IndustryID = GetItemID(DB_NAME, "Industry", [i+1])
+            category_datas = SelectItems(DB_NAME, "Category", "IndustryID,", [IndustryID,])
+            numCategorys = len(category_datas)
+            NewLocationDatas = []
+            region_name = ""
+            for idx, category_data in enumerate(category_datas):
+                CategoryID = category_data[0]
                 if len(NewLocationDatas) >= limite:
                     break
-                location_datas = SelectItems(DB_NAME, "Location", "CategoryID, RegionID", [CategoryID, RegionID])
-                if (len(location_datas) == 0):
-                    region_name = region_data[1]
-                    NewLocationDatas.append([CategoryID, region_data])
-        num_add = len(NewLocationDatas)
-        print (f"Industry {i+1:02d} {region_name} ... Number to add:\t{num_add}")
-        Q = multiprocessing.Queue()
-        P = multiprocessing.Process(target= run_dunbrad_spider, args= (NewLocationDatas, Q))
-        P.start()
-        P.join(timeout= num_add * 2 if num_add > 10 else 20)
-        LocationData = []
-        res = -1
-        while not Q.empty():
-            loc_data = Q.get(timeout= 5)
-            res = loc_data["result"]
-            regionID = loc_data["region"]
-            regionName = loc_data["region_name"]
-            categoryID = loc_data["category"]
-            loc = loc_data["data"]
-            if res == 0:
-                if len(loc) > 0:
-                    for loc_name in loc:
-                        url = loc[loc_name]
-                        LocationData.append((loc_name, url, categoryID, regionID))
-            elif res == 1:
-                url = loc
-                LocationData.append((regionName, url, categoryID, regionID))
-        print (f"Industry {i+1:02d} Location ... Num insert\t{len(LocationData)}")
-        INSERT(DB_NAME, "Location", LocationData)
-        Q.close()
-        Q.join_thread()
-        P.kill()
-        if num_add == 0 and res == 0:
-            break
+                region_datas = SelectItems(DB_NAME, "Region", "CategoryID,", [CategoryID,])
+                num_reg_datas = len(region_datas)
+                for reg_idx, region_data in enumerate(region_datas):
+                    RegionID = region_data[0]
+                    if len(NewLocationDatas) >= limite:
+                        break
+                    location_datas = SelectItems(DB_NAME, "Location", "CategoryID, RegionID", [CategoryID, RegionID])
+                    if (len(location_datas) == 0):
+                        region_name = region_data[1]
+                        NewLocationDatas.append([CategoryID, region_data])
+            num_add = len(NewLocationDatas)
+            print (f"Industry {i+1:02d} {region_name} ... Number to add:\t{num_add}")
+            Q = multiprocessing.Queue()
+            P = multiprocessing.Process(target= run_dunbrad_spider, args= (NewLocationDatas, Q))
+            P.start()
+            P.join(timeout= num_add * 2 if num_add > 10 else 20)
+            LocationData = []
+            res = -1
+            while not Q.empty():
+                loc_data = Q.get(timeout= 5)
+                res = loc_data["result"]
+                regionID = loc_data["region"]
+                regionName = loc_data["region_name"]
+                categoryID = loc_data["category"]
+                loc = loc_data["data"]
+                if res == 0:
+                    if len(loc) > 0:
+                        for loc_name in loc:
+                            url = loc[loc_name]
+                            LocationData.append((loc_name, url, categoryID, regionID))
+                elif res == 1:
+                    url = loc
+                    LocationData.append((regionName, url, categoryID, regionID))
+            print (f"Industry {i+1:02d} Location ... Num insert\t{len(LocationData)}")
+            INSERT(DB_NAME, "Location", LocationData)
+            Q.close()
+            Q.join_thread()
+            P.kill()
+            if num_add == 0:
+                break
